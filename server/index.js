853b6b243df54d3a18f9da86b1c074d833effd82
Probal Sen <9probalsen@gmail.com>
2025-06-19 23:29:59 +0530

    port installed

--- server/index.js
+++ server/index.js
@@ -66,7 +66,7 @@ app.use((err, req, res, next) => {
   });
 });
 
-const PORT = 5001;
+const PORT =process.env.PORT|| 5001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 }); 
